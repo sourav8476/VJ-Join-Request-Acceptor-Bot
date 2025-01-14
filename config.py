66fd0b1ba42d688951e3b13ac56b10e422434da1
sourav8476 <souravbosu2178@gmail.com>
2025-01-14 09:31:24 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "20679071"))
+API_HASH = environ.get("API_HASH", "3d088893c7ff5b84c429eadf6df88ab4")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-# Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002152956062"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002392980176"))
+ADMINS = int(environ.get("ADMINS", "5408428203"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_URI = environ.get("DB_URI", "mongodb+srv://barmanhailey:sSn6R9iYbtBEXtaw@cluster0.uricj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "autoacceptbot")
 
 # If this is True Then Bot Accept New Join Request 
-NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', False))
+NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
